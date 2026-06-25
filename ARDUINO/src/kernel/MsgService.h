f0ff2a5d702e1d0606b70c93b8c8c23edd71c969
ravag <alerava4@gmail.com>
2026-06-25 10:38:20 +0200

    Added MsgService

---
+++ ARDUINO/src/kernel/MsgService.h
@@ -0,0 +1,36 @@
+#ifndef __MSGSERVICE__
+#define __MSGSERVICE__
+
+#include "Arduino.h"
+
+class Msg {
+  String content;
+
+public:
+  Msg(String content){
+    this->content = content;
+  }
+  
+  String getContent(){
+    return content;
+  }
+};
+
+class MsgServiceClass {
+    
+public: 
+  
+  Msg* currentMsg;
+  bool msgAvailable;
+
+  void init();  
+
+  bool isMsgAvailable();
+  Msg* receiveMsg();
+  
+  void sendMsg(const String& msg);
+};
+
+extern MsgServiceClass MsgService;
+
+#endif
