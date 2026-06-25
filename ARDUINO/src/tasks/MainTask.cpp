d437f7a5d2868fabb1982e9b4470f7ae3de4085c
Marco Bulgarelli <marco.bulgarelli6@studio.unibo.it>
2026-06-25 11:31:17 +0200

    Added an asyncronous method to move the servo motor

--- ARDUINO/src/tasks/MainTask.cpp
+++ ARDUINO/src/tasks/MainTask.cpp
@@ -1,10 +1,16 @@
 #include "MainTask.h"
 
-MainTask::MainTask(ServoMotorImpl* servo, PotImpl* pot, SystemMode* mode, bool* valveState) {
+MainTask::MainTask(ServoMotorImpl* servo, PotImpl* pot, SystemMode* mode, bool* isManualInit) {
     this->servo = servo;
     this->pot = pot;
     this->currentMode = mode;
-    this->isServoConfiguredForManual = valveState;
+    this->isServoConfiguredForManual = isManualInit;
+
+    this->currentAngle = 0;
+    this->startAngle = 0;
+    this->startAngle = 0;
+    this->timeInState = 0;
+    this->timeToMove = 0;
 }
 
 void MainTask::tick() {
@@ -14,22 +20,50 @@ void MainTask::tick() {
             Serial.println("[DEBUG]: MANUAL Mode Activated");
             *isServoConfiguredForManual = true;
 
-            int inputPercentage = pot->getPercentage();
-            int angle = map(inputPercentage, 0, 100, 0, 90);
-            servo->setPosition(angle);
+            int potPercentage = pot->getPercentage();
+            currentAngle = map(potPercentage, 0, 100, 0, 90);
+            targetAngle = currentAngle;
+            startAngle = currentAngle;
+            timeInState = millis();
+            timeToMove = 0;
         }
 
-        if(pot->hasChanged()) {
-            int inputPercentage = pot->getPercentage();
-            int angle = map(inputPercentage, 0, 100, 0, 90);
-            servo->setPosition(angle);
+        int potPercentage = pot->getPercentage();
+        int newTarget = map(potPercentage, 0, 100, 0, 90);
 
-            Serial.print("[DEBUG]: Valve open: ");
-            Serial.print(inputPercentage);
-            Serial.print("%");
+        //Ho messo che si sposta solo se il potenziometro si sposta di almeno 2 gradi per evitare micro flicker analogici
+        if(abs(newTarget - targetAngle) >= 2) {
+            startAngle = currentAngle;
+            targetAngle = newTarget;
+            timeInState = millis();
+
+            unsigned long degreesToTravel = abs(targetAngle - currentAngle);
+            timeToMove = degreesToTravel * MS_PER_DEGREE;
+
+            //Evito divisioni per 0 se il movimento è per qualche motivo minore a 1
+            if(timeToMove == 0) {
+                timeToMove = 1;
+            }
+        }
+
+        unsigned long dt = millis() - timeInState;
+
+        if(dt <= timeToMove && currentAngle != targetAngle) {
+            float progress = (float)dt /timeToMove;
+
+            currentAngle = startAngle + (progress * (targetAngle - startAngle));
+            servo->setPosition(currentAngle);
+
+            Serial.print("[DEBUG]: Target: ");
+            Serial.print(targetAngle);
+            Serial.print("° | Current Angle: ");
+            Serial.print(currentAngle);
+            Serial.print("°");
             Serial.println("");
 
-            delay(50);
+        } else if(currentAngle != targetAngle) {
+            currentAngle = targetAngle;
+            servo->setPosition(currentAngle);
         }
     }
 }
