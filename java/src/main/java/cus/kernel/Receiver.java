56d807480267d71fc5be32d16df3c966feb83a0e
ravag <alerava4@gmail.com>
2026-07-07 11:00:23 +0200

    Modified java receiver

---
+++ java/src/main/java/cus/kernel/Receiver.java
@@ -0,0 +1,41 @@
+package cus.kernel;
+
+import cus.data.DataPoint;
+import cus.data.Mode;
+
+public class Receiver extends Thread {
+    private SerialCommunication serial;
+    private boolean stopped = false;
+    private String msg;
+    private DataPoint data;
+
+    public Receiver(SerialCommunication serial, DataPoint data) {
+        this.serial = serial;
+        this.data = data;
+        this.msg = "";
+    }
+
+    public void run() {
+        while (!stopped) {
+            msg = serial.readResponse();
+            if (msg != "") {
+                String[] spaced = msg.split(" ");
+                System.out.println(spaced);
+                data.setCurrentOpening(Integer.parseInt(spaced[1]));
+                if (!data.getState().equals(Mode.UNCONNECTED)) {
+                    data.setState(Mode.valueOf(spaced[3]));
+                }
+            }
+            
+            try {
+                Thread.sleep(250);
+            } catch (Exception e) {
+                System.err.println("WompWomp");
+            }
+        }
+    }
+
+    public void terminate() {
+        this.stopped = true;
+    }
+}
